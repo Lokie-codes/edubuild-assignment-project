fd62ade6fd0ce67c362056aa9b0ddba974ac6ca3
Lokesh <lokeshlucky13579@gmail.com>
2023-08-01 00:28:25 +0530

    Updation of tendor detail view

--- tender/views.py
+++ tender/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 from .models import Tender
 from .forms import TenderForm
+
 # Create your views here.
 
 class TenderListView(ListView):
@@ -10,17 +17,18 @@ class TenderListView(ListView):
     context_object_name = "tender_list"
     template_name = "tender/tender_list.html"
 
+
 class TenderDetailView(DetailView):
     model = Tender
     context_object_name = "tender"
     template_name = "tender/tender_detail.html"
+
 
 class TenderCreateView(CreateView):
     model = Tender
     fields = "__all__"
     template_name = "tender/tender_create.html"
 
+
 class TenderUpdateView(UpdateView):
     pass
-
-
